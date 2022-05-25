188a60cd83b1997879d7cb1834242b96fd44ce97
Jubair <jubairhossen55@gmail.com>
2022-05-25 21:22:42 +0600

    My Orders section added

--- src/Pages/Deshboard/MyOrder.js
+++ src/Pages/Deshboard/MyOrder.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Spinner from '../../Components/Spinner';
+import OrderRow from './OrderRow';
+import auth from '../../firebase.init';
 
 const MyOrder = () => {
+    const [user] = useAuthState(auth);
+    const { data: orders, isLoading, refetch } = useQuery('myOrders', () => fetch(`http://localhost:5000/myorders?email=${user.email}`, {
+        method: "GET",
+        headers: {
+            'authorization': `Beares ${localStorage.getItem('token')}`
+        }
+    })
+        .then(res => res.json()
+        ));
+
+    if (isLoading) {
+        return <Spinner></Spinner>
+    }
     return (
-        <div>
-            My order
+        <div className="overflow-x-auto">
+            <h1 className="text-xl my-3">My Orders</h1>
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>Sl</th>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>Total Price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        orders.map((order, index) => <OrderRow
+                            key={order._id}
+                            index={index}
+                            order={order}
+                            refetch={refetch}
+                        ></OrderRow>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
---
+++ src/Pages/Deshboard/OrderRow.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const OrderRow = ({ index, order, refetch }) => {
+    const { productName, orderQuantity, price, _id } = order;
+    const handleCancel = () => {
+        const confirm = window.confirm('Are You Sure? You Want to delete');
+        if (confirm) {
+            fetch(`http://localhost:5000/order/${_id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount) {
+                        refetch()
+                    }
+                })
+        }
+    }
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>{productName}</td>
+            <td>{orderQuantity}</td>
+            <td>${price}</td>
+            <td>
+                {
+                    order.paid ? <>
+                        <p className='text-gray-400'>paid</p>
+                    </>
+                        :
+                        <>
+                            <button onClick={handleCancel} className="btn btn-xs mr-3">Cancel</button>
+                            <button className="btn btn-xs">Pay</button>
+                        </>
+                }
+            </td>
+        </tr>
+    );
+};
+
+export default OrderRow;
